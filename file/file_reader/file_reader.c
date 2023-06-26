7f73c18fc147703eb15992e759d4fb1b9e1f2e42
Pedro Martineli <martinelipedro@gmail.com>
2023-06-26 11:19:01 -0300

    Started file reader

---
+++ file/file_reader/file_reader.c
@@ -0,0 +1,71 @@
+#include "file_reader.h"
+#include <stdio.h>
+#include <string.h>
+
+file_reader_T* init_file_reader(const char* filepath)
+{
+    file_reader_T* file_reader = calloc(1, sizeof(struct FILE_READER_STRUCT));
+
+    file_reader->filepath = filepath;
+    FILE* file = fopen(filepath, "rb");
+
+    if (file)
+    {
+        fseek(file, 0, SEEK_END);
+        file_reader->lenght = ftell(file);
+        fclose(file);
+        return file_reader;
+    }
+    
+    printf("File not found: %s\n", filepath);
+    exit(1);
+}
+
+char* file_reader_get_contents(file_reader_T* file_reader)
+{
+    FILE* file = fopen(file_reader->filepath, "rb");
+    char* buffer = calloc(file_reader->lenght, file_reader->lenght);
+    fread(buffer, 1, file_reader->lenght, file);
+    fclose(file);
+
+    return buffer;
+}
+
+size_t file_reader_get_line_count(file_reader_T* file_reader)
+{
+    FILE* file = fopen(file_reader->filepath, "rb");
+    size_t count = 0;
+
+    for (char c = getc(file); c != EOF; c = getc(file))
+    {
+        if (c == '\n')
+        {
+            count++;
+        }
+    }
+    count++;
+    fclose(file);
+
+    return count;
+}
+
+char** file_reader_get_lines_array(file_reader_T* file_reader)
+{
+    FILE* file = fopen(file_reader->filepath, "rb");
+    char** lines_array = calloc(file_reader_get_line_count(file_reader), sizeof(char*));
+    size_t current_line = 0;
+
+    for (char c = getc(file); c != EOF; c = getc(file))
+    {
+        lines_array[current_line] = realloc(lines_array[current_line], sizeof(lines_array[current_line]) + sizeof(char));
+        lines_array[current_line][strlen(lines_array[current_line])] = c;
+
+        if (c == '\n')
+        {
+            current_line++;
+        }
+    }
+
+    fclose(file);
+    return lines_array;
+}
---
+++ file/file_reader/file_reader.h
@@ -0,0 +1,19 @@
+#ifndef FILE_FILEREADER_H
+#define FILE_FILEREADER_H
+
+#include <stdlib.h>
+
+typedef struct FILE_READER_STRUCT
+{
+    const char* filepath;
+    long lenght;
+
+} file_reader_T;
+
+file_reader_T* init_file_reader(const char* filepath);
+
+char* file_reader_get_contents(file_reader_T* file_reader);
+size_t file_reader_get_line_count(file_reader_T* file_reader);
+char** file_reader_get_lines_array(file_reader_T* file_reader);
+
+#endif
